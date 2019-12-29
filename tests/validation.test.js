959b4c0dbc7c4083a8d5c2c4d8a650c5d3e76aa2
cheap-glitch <cheap.glitch@gmail.com>
2019-12-29 10:56:12 +0100

    Add code coverage

--- tests/validation.test.js
+++ tests/validation.test.js
@@ -10,6 +10,7 @@ const validateOptions = require('../src/validation');
 const validate = _options => validateOptions({ routes: [{ path: '/' }], ..._options});
 
 describe('validation of the options returns an error when:', () => {
+
 	/**
 	 * Meta
 	 * ---------------------------------------------------------------------
@@ -17,6 +18,7 @@ describe('validation of the options returns an error when:', () => {
 	it("there are extra properties on the main options object", () => {
 		expect(validate({ someProp: true })).not.to.be.null;
 	});
+
 	it("both routes and URLs are provided", () => {
 		expect(validateOptions({ urls: [{ loc: '/' }], routes: [{ path: '/' }] })).not.to.be.null;
 	});
@@ -33,20 +35,25 @@ describe('validation of the options returns an error when:', () => {
 		expect(validate({ baseURL: 'https://domain.fr' })).to.be.null;
 		expect(validate({ baseURL: 'http://www.other-domain.fr' })).to.be.null;
 	});
+
 	describe("the default URL params are invalid, because", () => {
+
 		it("'defaults' is not an object", () => {
 			expect(validate({ defaults: true })).not.to.be.null;
 			expect(validate({ defaults: 'weekly' })).not.to.be.null;
 		});
+
 		it("'defaults' has extraneous properties", () => {
 			expect(validate({ defaults: { loc: '/lorem/ipsum' } })).not.to.be.null;
 			expect(validate({ defaults: { path: '/lorem/ipsum' } })).not.to.be.null;
 			expect(validate({ defaults: { path: '/lorem/ipsum' } })).not.to.be.null;
 		});
+
 		it("'lastmod' is not a Date object or a string", () => {
 			expect(validate({ defaults: { lastmod: true } })).not.to.be.null;
 			expect(validate({ defaults: { lastmod: { date: '2012-12-21' } } })).not.to.be.null;
 		});
+
 		it("'lastmod' is an invalid Date object", () => {
 			expect(validate({ defaults: { lastmod: new Date('the first day of the universe') } })).not.to.be.null;
 			expect(validate({ defaults: { lastmod: new Date('last tuesday, when it was raining') } })).not.to.be.null;
@@ -55,6 +62,7 @@ describe('validation of the options returns an error when:', () => {
 			expect(validate({ defaults: { lastmod: new Date('2019-12-28') } })).to.be.null;
 			expect(validate({ defaults: { lastmod: new Date('2019-12-28T21:17:34') } })).to.be.null;
 		});
+
 		it("'lastmod' is an invalid date", () => {
 			expect(validate({ defaults: { lastmod: 'the first day of the universe' } })).not.to.be.null;
 			expect(validate({ defaults: { lastmod: 'last tuesday, when it was raining' } })).not.to.be.null;
@@ -63,6 +71,7 @@ describe('validation of the options returns an error when:', () => {
 			expect(validate({ defaults: { lastmod: '2019-12-28' } })).to.be.null;
 			expect(validate({ defaults: { lastmod: '2019-12-28T21:17:34' } })).to.be.null;
 		});
+
 		it("'changefreq' is not a valid value", () => {
 			expect(validate({ defaults: { changefreq: 25 } })).not.to.be.null;
 			expect(validate({ defaults: { changefreq: 'often' } })).not.to.be.null;
@@ -72,6 +81,7 @@ describe('validation of the options returns an error when:', () => {
 			expect(validate({ defaults: { changefreq: 'monthly' } })).to.be.null;
 			expect(validate({ defaults: { changefreq: 'never' } })).to.be.null;
 		});
+
 		it("'priority' is not a valid value", () => {
 			expect(validate({ defaults: { priority: 'high' } })).not.to.be.null;
 			expect(validate({ defaults: { priority: 100 } })).not.to.be.null;
@@ -88,13 +98,29 @@ describe('validation of the options returns an error when:', () => {
 	});
 
 	/**
-	 * URLs
+	 * Routes
 	 * ---------------------------------------------------------------------
 	 */
-	// @TODO
+	describe("the routes are invalid, because", () => {
+
+		it("'routes' is not an array", () => {
+			expect(validateOptions({ routes: {} })).not.to.be.null;
+			expect(validateOptions({ routes: true })).not.to.be.null;
+		});
+
+		it("there is a route with no 'path' property", () => {
+			expect(validateOptions({ routes: [{}] })).not.to.be.null;
+			expect(validateOptions({ routes: [{ changefreq: 'weekly' }] })).not.to.be.null;
+			expect(validateOptions({ routes: [{ path: '/' }, {}] })).not.to.be.null;
+			expect(validateOptions({ routes: [{ path: '/' }, { changefreq: 'weekly' }] })).not.to.be.null;
+
+			expect(validateOptions({ routes: [{ path: '/' }] })).to.be.null;
+			expect(validateOptions({ routes: [{ path: '/' }, { path: '/about' }] })).to.be.null;
+		});
+	});
 
 	/**
-	 * Routes
+	 * URLs
 	 * ---------------------------------------------------------------------
 	 */
 	// @TODO
