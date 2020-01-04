2b43e53c378be13ec2ca4f38be536ac8773dca04
cheap-glitch <cheap.glitch@gmail.com>
2020-01-04 20:19:58 +0100

    Supplement tests for the generation of the sitemap

--- tests/sitemap.test.js
+++ tests/sitemap.test.js
@@ -13,16 +13,17 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 	/**
 	 * URLs
+	 * {{{
 	 * ---------------------------------------------------------------------
 	 */
 	describe("from an array of URLs", () => {
 
 		it("generates a simple sitemap from full URLs", () => {
 			expect(generateSitemapXML({
-				baseURL:  '',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: 'https://website.net' }, { loc: 'https://website.net/about' }],
+				baseURL:   '',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: 'https://website.net' }, { loc: 'https://website.net/about' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net</loc></url><url><loc>https://website.net/about</loc></url>`
 			));
@@ -30,10 +31,10 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("generates a simple sitemap from partial URLs and a base URL", () => {
 			expect(generateSitemapXML({
-				baseURL:  'https://website.net',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: '/' }, { loc: '/about' }],
+				baseURL:   'https://website.net',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: '/' }, { loc: '/about' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net</loc></url><url><loc>https://website.net/about</loc></url>`
 			));
@@ -41,10 +42,10 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("removes trailing slashes", () => {
 			expect(generateSitemapXML({
-				baseURL:  'https://website.net',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: '/' }, { loc: '/about' }, { loc: '/page/' }],
+				baseURL:   'https://website.net',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: '/' }, { loc: '/about' }, { loc: '/page/' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net</loc></url><url><loc>https://website.net/about</loc></url><url><loc>https://website.net/page</loc></url>`
 			));
@@ -52,10 +53,10 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("adds trailing slashes if the 'trailingSlash' option is set", () => {
 			expect(generateSitemapXML({
-				baseURL:  'https://website.net',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: '/' }, { loc: '/about' }, { loc: '/page/' }],
+				baseURL:   'https://website.net',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: '/' }, { loc: '/about' }, { loc: '/page/' }],
 				trailingSlash: true,
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net/</loc></url><url><loc>https://website.net/about/</loc></url><url><loc>https://website.net/page/</loc></url>`
@@ -64,31 +65,30 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("encodes URIs properly", () => {
 			expect(generateSitemapXML({
-				baseURL:  'https://website.net',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: '/search?color="always"&reverse-order' }],
+				baseURL:   'https://website.net',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: '/search?color="always"&reverse-order' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net/search?color=%22always%22&amp;reverse-order</loc></url>`
 			));
 
 			expect(generateSitemapXML({
-				baseURL:  'https://éléphant.net',
-				defaults: {},
-				routes:   [],
-				urls:     [{ loc: '/about' }],
+				baseURL:   'https://éléphant.net',
+				defaults:  {},
+				routes:    [],
+				urls:      [{ loc: '/about' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://%C3%A9l%C3%A9phant.net/about</loc></url>`
 			));
 		});
 
-
 		it("takes per-URL parameters into account", () => {
 			expect(generateSitemapXML({
-				baseURL:  '',
-				defaults: {},
-				routes:   [],
-				urls:     [{
+				baseURL:   '',
+				defaults:  {},
+				routes:    [],
+				urls:      [{
 					loc:         'https://website.net/about',
 					changefreq:  'monthly',
 					lastmod:     '2020-01-01',
@@ -101,14 +101,14 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("takes default URL parameters into account", () => {
 			expect(generateSitemapXML({
-				baseURL:  '',
-				defaults: {
+				baseURL:   '',
+				defaults:  {
 					changefreq:  'monthly',
 					lastmod:     '2020-01-01',
 					priority:    0.3,
 				},
-				routes:   [],
-				urls:     [{
+				routes:    [],
+				urls:      [{
 					loc:         'https://website.net/about',
 				}]
 			})).to.equal(wrapURLs(
@@ -118,13 +118,13 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 
 		it("prioritizes per-URL parameters over global defaults", () => {
 			expect(generateSitemapXML({
-				baseURL:  '',
-				defaults: {
+				baseURL:   '',
+				defaults:  {
 					changefreq:  'never',
 					priority:    0.8,
 				},
-				routes:   [],
-				urls:     [{
+				routes:    [],
+				urls:      [{
 					loc:         'https://website.net/about',
 					changefreq:  'monthly',
 					lastmod:     '2020-01-01',
@@ -137,21 +137,167 @@ describe("vue-cli-plugin-sitemap sitemap generation", () => {
 	});
 
 	/**
+	 * }}}
+	 */
+
+	/**
 	 * Routes
+	 * {{{
 	 * ---------------------------------------------------------------------
 	 */
 	describe("from an array of routes", () => {
 
-		/*
 		it("generates a sitemap from simple routes", () => {
 			expect(generateSitemapXML({
-				baseURL:  'https://website.net',
-				defaults: {},
-				routes:   [{ path: '/' }, { path: '/about' }],
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{ path: '/' }, { path: '/about' }],
 			})).to.equal(wrapURLs(
 				`<url><loc>https://website.net</loc></url><url><loc>https://website.net/about</loc></url>`
 			));
 		});
-		*/
+
+		it("removes trailing slashes", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{ path: '/' }, { path: '/about' }, { path: '/page/' }],
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net</loc></url><url><loc>https://website.net/about</loc></url><url><loc>https://website.net/page</loc></url>`
+			));
+		});
+
+		it("adds trailing slashes if the 'trailingSlash' option is set", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{ path: '/' }, { path: '/about' }, { path: '/page/' }],
+				trailingSlash: true,
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/</loc></url><url><loc>https://website.net/about/</loc></url><url><loc>https://website.net/page/</loc></url>`
+			));
+		});
+
+		it("takes per-route URL parameters into account", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{
+					path:        '/about',
+					changefreq:  'monthly',
+					lastmod:     '2020-01-01',
+					priority:    0.3,
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/about</loc><lastmod>2020-01-01</lastmod><changefreq>monthly</changefreq><priority>0.3</priority></url>`
+			));
+
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{
+					path: '/about',
+					sitemap: {
+						changefreq:  'monthly',
+						lastmod:     '2020-01-01',
+						priority:    0.3,
+					}
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/about</loc><lastmod>2020-01-01</lastmod><changefreq>monthly</changefreq><priority>0.3</priority></url>`
+			));
+		});
+
+		it("takes default URL parameters into account", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {
+					changefreq:  'monthly',
+					lastmod:     '2020-01-01',
+					priority:    0.3,
+				},
+				urls:      [],
+				routes:    [{
+					path: '/about',
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/about</loc><lastmod>2020-01-01</lastmod><changefreq>monthly</changefreq><priority>0.3</priority></url>`
+			));
+		});
+
+		it("prioritizes per-route URL parameters over global defaults", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {
+					changefreq:  'never',
+					priority:    0.8,
+				},
+				urls:      [],
+				routes:    [{
+					path:        '/about',
+					changefreq:  'monthly',
+					lastmod:     '2020-01-01',
+					priority:    0.3,
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/about</loc><lastmod>2020-01-01</lastmod><changefreq>monthly</changefreq><priority>0.3</priority></url>`
+			));
+		});
+
+		it("generates an URL for each slug", () => {
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{
+					path:  '/article/:title',
+					slugs: [
+						'my-first-article',
+						'3-tricks-to-better-fold-your-socks',
+					]
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/article/my-first-article</loc></url><url><loc>https://website.net/article/3-tricks-to-better-fold-your-socks</loc></url>`
+			));
+
+			expect(generateSitemapXML({
+				baseURL:   'https://website.net',
+				defaults:  {},
+				urls:      [],
+				routes:    [{
+					path:  '/article/:title',
+					sitemap: {
+						slugs: [
+							'my-first-article',
+							'3-tricks-to-better-fold-your-socks',
+						]
+					}
+				}]
+			})).to.equal(wrapURLs(
+				`<url><loc>https://website.net/article/my-first-article</loc></url><url><loc>https://website.net/article/3-tricks-to-better-fold-your-socks</loc></url>`
+			));
+		});
 	});
+
+	/**
+	 * }}}
+	 */
+
+	/**
+	 * Routes + URLs
+	 * {{{
+	 * ---------------------------------------------------------------------
+	 */
+	describe("from both routes and URLs", () => {
+		// @TODO
+	});
+
+	/**
+	 * }}}
+	 */
 });
